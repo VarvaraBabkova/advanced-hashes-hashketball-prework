58c50cc3b53d7f2624d23905ba6a24dfa0c56231
Varvara Babkova <varvara.munday@gmail.com>
2019-06-10 16:44:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -143,6 +143,12 @@ def shoe_size (name)
   end
 end
 
+def team_colors (name)
+  game_hash.each do |team, info|
+    return team[:colors] if team[:team_name] == name
+  end
+end
+
 def good_practices
   game_hash.each do |location, team_data|
     #are you ABSOLUTELY SURE what 'location' and 'team data' are? use binding.pry to find out!
