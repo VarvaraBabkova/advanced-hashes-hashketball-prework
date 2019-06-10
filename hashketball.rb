f5c3ce1025896fbfcfbe2e79925883493f48fe2c
Varvara Babkova <varvara.munday@gmail.com>
2019-06-10 16:53:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -157,19 +157,12 @@ def team_names
   return ar
 end
 
-def good_practices
-  game_hash.each do |location, team_data|
-    #are you ABSOLUTELY SURE what 'location' and 'team data' are? use binding.pry to find out!
-    binding.pry
-    team_data.each do |attribute, data|
-      #are you ABSOLUTELY SURE what 'attribute' and 'team data' are? use binding.pry to find out!
-      binding.pry
-
-      #what is 'data' at each loop throughout .each block? when will the following line of code work and when will it break?
-      data.each do |data_item|
-          binding.pry
-      end
+def player_numbers (team_str)
+  ar = []
+  game_hash.each do |team, info|
+    game_hash[team][:players].each do |player|
+      ar.push(player[:number]) if info[:team_name] == name
     end
   end
+  return ar
 end
-#good_practices
