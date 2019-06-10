9e488acbb16dec2094ed318e26d3306ced522971
Varvara Babkova <varvara.munday@gmail.com>
2019-06-10 16:46:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -149,6 +149,10 @@ def team_colors (name)
   end
 end
 
+def team_names (team)
+
+end
+
 def good_practices
   game_hash.each do |location, team_data|
     #are you ABSOLUTELY SURE what 'location' and 'team data' are? use binding.pry to find out!
